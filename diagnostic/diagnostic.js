768c176a079ffd1696b52316fb05525bde2eccfe
jonmpalmer <palmejon@oregonstate.edu>
2020-06-01 16:49:54 -0700

    Update diagnostic.js

--- diagnostic/diagnostic.js
+++ diagnostic/diagnostic.js
@@ -1,37 +1,18 @@
 var express = require('express');
 var mysql = require('./dbcon.js');
+var bodyParser = require('body-parser');
 
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
 app.engine('handlebars', handlebars.engine);
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/static', express.static('public'));
 app.set('view engine', 'handlebars');
 app.set('port', process.argv[2]);
+app.set('mysql', mysql);
 
-app.get('/',function(req,res,next){
-  var context = {};
-  var createString = "CREATE TABLE diagnostic(" +
-  "id INT PRIMARY KEY AUTO_INCREMENT," +
-  "text VARCHAR(255) NOT NULL)";
-  mysql.pool.query('DROP TABLE IF EXISTS diagnostic', function(err){
-    if(err){
-      next(err);
-      return;
-    }
-    mysql.pool.query(createString, function(err){
-      if(err){
-        next(err);
-    return;
-      }
-    mysql.pool.query('INSERT INTO diagnostic (`text`) VALUES ("MySQL is Working!")',function(err){
-      mysql.pool.query('SELECT * FROM diagnostic', function(err, rows, fields){
-      context.results = JSON.stringify(rows);
-      res.render('home',context);
-    });
-    });
-    });
-  });
-});
+app.use('/people', require('./people.js'));
 
 app.use(function(req,res){
   res.status(404);
