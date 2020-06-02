8057e79010e7342829658ce936e7429d1d73bb21
jonmpalmer <palmejon@oregonstate.edu>
2020-06-01 20:42:19 -0700

    Update orders.js

--- diagnostic/orders.js
+++ diagnostic/orders.js
@@ -1,1 +1,99 @@
+module.exports = function(){
+    var express = require('express');
+    var router = express.Router();
 
+    function getOrder(res, mysql, context, complete){
+        mysql.pool.query("", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.customer = results;
+            complete();
+        });
+    }
+
+    /*Display all orders */
+
+    router.get('/', function(req, res){
+        var callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["deleteorder.js"];
+        var mysql = req.app.get('mysql');
+        getOrder(res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 2){
+                res.render('customer', context);
+            }
+
+        }
+    });
+
+    router.get('/:id', function(req, res){
+        callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["selectedorder.js", "updateorder.js"];
+        var mysql = req.app.get('mysql');
+        getOrder(res, mysql, context, req.params.id, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 2){
+                res.render('update-order', context);
+            }
+
+        }
+    });
+
+    /* Adds an order */
+
+    router.post('/', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "INSERT INTO  VALUES (?,?,?,?,?)";
+        var inserts = [req.body.addressID, req.body.firstName, req.body.lastName, req.body.phone, req.body.email];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.redirect('/customers');
+            }
+        });
+    });
+
+    /* The URI that update data is sent to in order to update an order */
+
+    router.put('/:id', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "UPDATE Orders";
+        var inserts = [req.body.firstName, req.body.lastName, req.body.phone, req.body.email, req.params.id];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.status(200);
+                res.end();
+            }
+        });
+    });
+
+    /* Route to delete a person, simply returns a 202 upon success. */
+
+    router.delete('/:id', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "DELETE FROM orders WHERE id = ?";
+        var inserts = [req.params.id];
+        sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.status(400);
+                res.end();
+            }else{
+                res.status(202).end();
+            }
+        })
+    })
+
+    return router;
+}();
