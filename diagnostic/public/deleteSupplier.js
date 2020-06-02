53810ebcc91f0c3b78a9c09aa4697c63004691ff
jonmpalmer <palmejon@oregonstate.edu>
2020-06-01 20:54:26 -0700

    Update deleteSupplier.js

--- diagnostic/public/deleteSupplier.js
+++ diagnostic/public/deleteSupplier.js
@@ -1,1 +1,9 @@
-
+function deleteSupplier(id){
+    $.ajax({
+        url: '/suppliers/' + id,
+        type: 'DELETE',
+        success: function(result){
+            window.location.reload(true);
+        }
+    })
+};
