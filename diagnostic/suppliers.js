4fef546f67308ec841f1042d69bad17a982b5573
jonmpalmer <palmejon@oregonstate.edu>
2020-06-01 20:52:23 -0700

    Update suppliers.js

--- diagnostic/suppliers.js
+++ diagnostic/suppliers.js
@@ -1,1 +1,99 @@
+module.exports = function(){
+    var express = require('express');
+    var router = express.Router();
 
+    function getSupplier(res, mysql, context, complete){
+        mysql.pool.query("", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.supplier = results;
+            complete();
+        });
+    }
+
+    /*Display all suppliers */
+
+    router.get('/', function(req, res){
+        var callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["deleteSupplier.js"];
+        var mysql = req.app.get('mysql');
+        getSupplier(res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 2){
+                res.render('supplier', context);
+            }
+
+        }
+    });
+
+    router.get('/:id', function(req, res){
+        callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["selectedSupplier.js", "updateSupplier.js"];
+        var mysql = req.app.get('mysql');
+        getSupplier(res, mysql, context, req.params.id, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 2){
+                res.render('update-supplier', context);
+            }
+
+        }
+    });
+
+    /* Adds an supplier */
+
+    router.post('/', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "INSERT INTO  VALUES (?,?,?,?,?)";
+        var inserts = [req.body.addressID, req.body.firstName, req.body.lastName, req.body.phone, req.body.email];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.redirect('/supplers');
+            }
+        });
+    });
+
+    /* The URI that update data is sent to in order to update an suppler */
+
+    router.put('/:id', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "UPDATE supplers";
+        var inserts = [req.body.firstName, req.body.lastName, req.body.phone, req.body.email, req.params.id];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.status(200);
+                res.end();
+            }
+        });
+    });
+
+    /* Route to delete an suppler*/
+
+    router.delete('/:id', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "DELETE FROM supplers WHERE id = ?";
+        var inserts = [req.params.id];
+        sql = mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.status(400);
+                res.end();
+            }else{
+                res.status(202).end();
+            }
+        })
+    })
+
+    return router;
+}();
