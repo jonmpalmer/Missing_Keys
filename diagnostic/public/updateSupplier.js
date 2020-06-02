c728fecc333c1c05179a5b9213d7d2014164c13a
jonmpalmer <palmejon@oregonstate.edu>
2020-06-01 20:56:04 -0700

    Update updateSupplier.js

--- diagnostic/public/updateSupplier.js
+++ diagnostic/public/updateSupplier.js
@@ -1,1 +1,10 @@
-
+function updateSupplier(id){
+    $.ajax({
+        url: '/suppliers/' + id,
+        type: 'PUT',
+        data: $('#update-supplier').serialize(),
+        success: function(result){
+            window.location.replace("./");
+        }
+    })
+};
