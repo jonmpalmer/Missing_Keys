50df9875ba9cdf9b27c66fb26c65554b8a1ae7e8
jonmpalmer <palmejon@oregonstate.edu>
2020-06-11 13:27:28 -0700

    Update dbcon.js

--- diagnostic/dbcon.js
+++ diagnostic/dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs340_palmejon',
-  password        : '****',
+  password        : '3293',
   database        : 'cs340_palmejon'
 });
 
