ed219d448095092bc77c1a6b65851393701a173a
jonmpalmer <palmejon@oregonstate.edu>
2020-06-11 16:45:45 -0700

    Update products.js

--- diagnostic/products.js
+++ diagnostic/products.js
@@ -2,13 +2,26 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
-    function getProduct(res, mysql, context, complete){
-        mysql.pool.query("SELECT Products.name, Products.type, Products.model, Products.description, Products.quant, Products.price, Products.image, Suppliers.supplierID, Suppliers.name FROM Products, Suppliers WHERE (Products.supplierID = Suppliers.supplierID", function(error, results, fields){
+    function getProducts(res, mysql, context, complete){
+        mysql.pool.query("SELECT name, type, model, description, quant, price", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
             }
-            context.product = results;
+            context.products = results;
+            complete();
+        });
+    }
+        
+    function getProduct(res, mysql, context, customerID, complete){
+        var sql = "SELECT name, type, model, description, quant, price FROM Products WHERE productID = ?";
+        var inserts = [productID];
+        mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.customer = results[0];
             complete();
         });
     }
@@ -20,28 +33,19 @@ module.exports = function(){
         var context = {};
         context.jsscripts = ["deleteProduct.js"];
         var mysql = req.app.get('mysql');
-        getOrder(res, mysql, context, complete);
+        getProducts(res, mysql, context, complete);
         function complete(){
-            callbackCount++;
-            if(callbackCount >= 2){
-                res.render('product', context);
-            }
-
+            res.render('product', context);
         }
     });
 
-    router.get('/:id', function(req, res){
-        callbackCount = 0;
+    router.get('/:productID', function(req, res){
         var context = {};
         context.jsscripts = ["selectedProduct.js", "updateProduct.js"];
         var mysql = req.app.get('mysql');
-        getOrder(res, mysql, context, req.params.id, complete);
+        getProduct(res, mysql, context, req.params.productID, complete);
         function complete(){
-            callbackCount++;
-            if(callbackCount >= 2){
-                res.render('update-product', context);
-            }
-
+            res.render('update-product', context);
         }
     });
 
@@ -49,8 +53,8 @@ module.exports = function(){
 
     router.post('/', function(req, res){
         var mysql = req.app.get('mysql');
-        var sql = "INSERT INTO Products (supplierID, name, type, model, description, quant, price, image) VALUES (?,?,?,?,?,?,?,?)";
-        var inserts = [req.body.supplierID, req.body.name, req.body.type, req.body.model, req.body.descrpition, req.body.quant, req.body.price, req.body.image];
+        var sql = "INSERT INTO Products (productID, supplierID, name, type, model, description, quant, price) VALUES (?,?,?,?,?,?,?,?)";
+        var inserts = [req.body.productID, req.body.supplierID, req.body.name, req.body.type, req.body.model, req.body.descrpition, req.body.quant, req.body.price];
         sql = mysql.pool.query(sql,inserts,function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
@@ -63,10 +67,10 @@ module.exports = function(){
 
     /* The URI that update data is sent to in order to update a product */
 
-    router.put('/:id', function(req, res){
+    router.put('/:productID', function(req, res){
         var mysql = req.app.get('mysql');
-        var sql = "UPDATE Products SET name = ?, type = ?, model = ?, description = ?, quant = ?, price = ?, image = ? WHERE productsID = ?;";
-        var inserts = [req.body.name, req.body.type, req.body.model, req.body.description, req.body.quant, req.body.price,req.body.image req.params.id];
+        var sql = "UPDATE Products SET name = ?, type = ?, model = ?, description = ?, quant = ?, price = ? WHERE productID = ?;";
+        var inserts = [req.body.name, req.body.type, req.body.model, req.body.description, req.body.quant, req.body.price, req.params.productID];
         sql = mysql.pool.query(sql,inserts,function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
@@ -80,10 +84,10 @@ module.exports = function(){
 
     /* Route to delete a product*/
 
-    router.delete('/:id', function(req, res){
+    router.delete('/:productID', function(req, res){
         var mysql = req.app.get('mysql');
-        var sql = "DELETE FROM products WHERE id = ?";
-        var inserts = [req.params.id];
+        var sql = "DELETE FROM Products WHERE productID = ?";
+        var inserts = [req.params.productID];
         sql = mysql.pool.query(sql, inserts, function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
