6b55aac78d36cd0711dae0907a0fa79ae79a37ec
jonmpalmer <palmejon@oregonstate.edu>
2020-06-11 21:12:15 -0700

    Update deleteCustomer.js

--- diagnostic/public/deleteCustomer.js
+++ diagnostic/public/deleteCustomer.js
@@ -1,6 +1,6 @@
-function deleteCustomer(customerID){
+function deleteCustomer(id){
     $.ajax({
-        url: '/customers/' + customerID,
+        url: '/customers/' + id,
         type: 'DELETE',
         success: function(result){
             window.location.reload(true);
