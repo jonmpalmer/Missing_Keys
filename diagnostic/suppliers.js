8a60bd32dfa4032271e6f976c8f121d01d3b7fe0
jonmpalmer <palmejon@oregonstate.edu>
2020-06-11 21:32:19 -0700

    Update suppliers.js

--- diagnostic/suppliers.js
+++ diagnostic/suppliers.js
@@ -44,7 +44,7 @@ module.exports = function(){
         var mysql = req.app.get('mysql');
         getSupplier(res, mysql, context, req.params.supplierID, complete);
         function complete(){
-            res.render('update-supplier', context);
+            res.render('update-suppliers', context);
         }
     });
 
